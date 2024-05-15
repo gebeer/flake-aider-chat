c57bf98bd240cd95b44d7a1c5a6a6c9cee30dc6b
gebeer <gerhard.sitzmann@gmail.com>
2024-05-15 16:56:33 +0700

    fix: libcc error, documentation
    add more descriptive description

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
-  description = "My Python Flake Shells";
-
+  description = "Flake providing dev shell for using aider-chat in NixOS";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
@@ -14,12 +13,18 @@
         buildInputs = with pkgs; [
           (pkgs.python3.withPackages (ps: with ps; [ virtualenv pip setuptools wheel ]))
         ];
+        # set LD_LIBRARY_PATH environment variable to avoid error. see https://discourse.nixos.org/t/how-to-solve-libstdc-not-found-in-shell-nix/25458
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib";
         shellHook = ''
+        # create virtualenv if not exist
           if [ ! -d .venv ]; then
             virtualenv .venv
           fi
+          # activate virtualenv
           source .venv/bin/activate
+          # install aider-chat into virtualenv and upgrade it
           pip install --upgrade aider-chat --prefix=$PWD/.venv
+          # export PATH to virtualenv bins
           export PATH=$PWD/.venv/bin:$PATH
         '';
         exitHook = ''
